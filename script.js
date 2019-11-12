dd6a4e05a5eedc480a4713e288537fa3ce550ead
Jonathan Luis Martinez <jonathan.martinez.305@my.csun.edu>
2019-11-12 01:03:00 -0800

    dynamically adds city button and displays temp in F

--- script.js
+++ script.js
@@ -1,32 +1,45 @@
 var apiKey = "&APPID=2401b6ee83614d604c72516b9ca97dde";
-var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=";
+
 
 $("#enterCity").on("click", function(event) {
 	event.preventDefault();
-
+	var queryURL = "";
 	var city = $("#cityInput").val().trim();
-	queryURL = queryURL + city + apiKey; 
+
+	queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + apiKey; 
 
   $.ajax({
 	url: queryURL,
 	method: "GET"
   }).then(function(response) {
-	  console.log(response);
+	  //console.log(response);
+
 	  var  strTemp = JSON.stringify(response.main.temp);
+	  var numTemp = parseFloat(strTemp);
+	  numTemp = numTemp - 273.15;
+	  numTemp = numTemp * (9/5);
+	  numTemp = numTemp + 32;
+	  numTemp = Number(numTemp.toFixed(1));
+
 	  var  strHumd= JSON.stringify(response.main.humidity);
 	  var  strWind= JSON.stringify(response.wind.speed);
 	  //var strIndx = response.uv.index;
 
+	
 	$("#locCont").append("City: " + city);
-	$("#temCont").append("Temperature: " + strTemp);
-	$("#humCont").append("Humidity: " + strHumd);
-	$("#winCont").append("Wind Speed " + strWind);
+	$("#temCont").append("Temperature: " + numTemp + " F");
+	$("#humCont").append("Humidity: " + strHumd + "%");
+	$("#winCont").append("Wind Speed " + strWind + " MPH");
 	//$("#indCont").append("UV Index " + strIndx);
 
-
-
-
+	cityButton();
+	
   });
 
-  
+  function cityButton(){
+	var newBttn = $("<button>");
+	newBttn.text(city);
+	$("#bttnContainer").append(newBttn);
+  }
 });
+
