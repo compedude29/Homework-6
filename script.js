9adafcd345c840846f2248e007427a73cad81fef
Jonathan Luis Martinez <jonathan.martinez.305@my.csun.edu>
2019-11-11 23:15:28 -0800

    search button triggers API for city typed

--- script.js
+++ script.js
@@ -1,14 +1,32 @@
+var apiKey = "&APPID=2401b6ee83614d604c72516b9ca97dde";
+var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=";
+
 $("#enterCity").on("click", function(event) {
 	event.preventDefault();
-	var city = $("#cityInput").val().trim();
-	console.log("The city you entered: " + city);
-  });
 
-  //API key: 2401b6ee83614d604c72516b9ca97dde
-  //api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=2401b6ee83614d604c72516b9ca97dde 
+	var city = $("#cityInput").val().trim();
+	queryURL = queryURL + city + apiKey; 
+
   $.ajax({
-	url: "https://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=2401b6ee83614d604c72516b9ca97dde",
+	url: queryURL,
 	method: "GET"
   }).then(function(response) {
-	console.log(response);
+	  console.log(response);
+	  var  strTemp = JSON.stringify(response.main.temp);
+	  var  strHumd= JSON.stringify(response.main.humidity);
+	  var  strWind= JSON.stringify(response.wind.speed);
+	  //var strIndx = response.uv.index;
+
+	$("#locCont").append("City: " + city);
+	$("#temCont").append("Temperature: " + strTemp);
+	$("#humCont").append("Humidity: " + strHumd);
+	$("#winCont").append("Wind Speed " + strWind);
+	//$("#indCont").append("UV Index " + strIndx);
+
+
+
+
   });
+
+  
+});
